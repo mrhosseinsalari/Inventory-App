dfa56e4ceeff8aef6768eaba35cadbb9ec23a608
Hossein Salari <hosein.salari81@gmail.com>
2023-11-28 15:07:43 +0330

    responsive app

--- public/src/js/ProductView.js
+++ public/src/js/ProductView.js
@@ -54,18 +54,21 @@ class ProductView {
       );
 
       result += `
-        <div class="flex items-center justify-between">
+        <div class="flex items-center justify-between text-xs min-[425px]:text-sm sm:text-base">
           <span class="text-slate-400">${product.title}</span>
-          <div class="flex items-center gap-x-3">
-            <span class="text-slate-400">${new Date(
+          <div class="flex items-center gap-x-1.5 min-[425px]:gap-x-3">
+            <span class="text-slate-400 hidden sm:block">${new Date(
               product.createdAt
-            ).toLocaleDateString("fa-IR")}</span>
-            <span class="block px-3 py-0.5 text-slate-400 border border-slate-400 rounded-2xl text-sm">${
+            ).toLocaleDateString()}</span>
+            <span class="block px-3 py-0.5 text-slate-400 border border-slate-400 rounded-2xl">${
               selectedCategory.title
             }</span>
-            <span class="flex items-center justify-center w-7 h-7 rounded-full bg-slate-500 border-2 border-slate-400 text-slate-300">${
+            <span class="flex items-center justify-center w-6 h-6 min-[425px]:w-7 min-[425px]:h-7 rounded-full bg-slate-500 border-2 border-slate-400 text-slate-300">${
               product.quantity
             }</span>
+            <button class="edit-product border px-2 py-0.5 rounded-2xl border-yellow-400 text-yellow-400" data-product-id="${
+              product.id
+            }">edit</button>
             <button class="delete-product border px-2 py-0.5 rounded-2xl border-red-400 text-red-400" data-product-id="${
               product.id
             }">delete</button>
